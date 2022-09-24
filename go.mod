c83adadfd2566e790b3d9874b547b35c48ff21db
Philipp Emanuel Weidmann <pew@worldwidemann.com>
2022-09-24 09:50:56 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module shin
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2022  Philipp Emanuel Weidmann <pew@worldwidemann.com>
+
+package main
+
+func main() {
+}
