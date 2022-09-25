ca23d028b01865588a6bf234f069f42ff726dff6
Philipp Emanuel Weidmann <pew@worldwidemann.com>
2022-09-25 11:16:26 +0530

    Add IBus engine skeleton

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module shin
 
 go 1.18
+
+require github.com/BambooEngine/goibus v0.0.0-20200430030917-ea4df577c5d6
+
+require github.com/godbus/dbus v4.1.0+incompatible
--- main.go
+++ main.go
@@ -3,5 +3,48 @@
 
 package main
 
+import (
+	"fmt"
+
+	"github.com/BambooEngine/goibus/ibus"
+	"github.com/godbus/dbus"
+)
+
+const engineBasePath = "/org/freedesktop/IBus/Engine/Shin"
+const busName = "org.freedesktop.IBus.Shin"
+
+type engine struct {
+	ibus.Engine
+}
+
+func (e *engine) ProcessKeyEvent(keyval uint32, keycode uint32, state uint32) (bool, *dbus.Error) {
+	fmt.Printf("Shin: ProcessKeyEvent(keyval = %v, keycode = %v, state = %v)\n", keyval, keycode, state)
+
+	return false, nil
+}
+
 func main() {
+	fmt.Println("Shin: Starting")
+
+	bus := ibus.NewBus()
+	connection := bus.GetDbusConn()
+
+	engineId := 0
+
+	ibus.NewFactory(connection, func(connection *dbus.Conn, engineName string) dbus.ObjectPath {
+		engineId++
+
+		path := dbus.ObjectPath(fmt.Sprintf("%v/%v", engineBasePath, engineId))
+		engine := &engine{ibus.BaseEngine(connection, path)}
+
+		ibus.PublishEngine(connection, path, engine)
+
+		return path
+	})
+
+	bus.RequestName(busName, 0)
+
+	fmt.Println("Shin: Started")
+
+	select {}
 }
